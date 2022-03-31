3a0ab73b128c474afc44bee53bc2a013fd790d38
Chandana-Jammigumpula <101253660+Chandana-Jammigumpula@users.noreply.github.com>
2022-03-31 16:05:10 +0530

    M1_telecom_billing_system_final_project

---
+++ delete.c
@@ -0,0 +1,51 @@
+//LEARNPROGRAMO-PRORAMMING MADE SIMPLE
+#include<stdio.h>
+#include<conio.h>
+#include<ctype.h>
+#include<windows.h>
+#include<stdlib.h>
+#include "delete.h"
+
+
+
+
+int main()
+{	int password;
+	int phonenumber;
+	char choice;
+
+
+
+	system("cls");
+
+	printf("\n\n\n\n\n\n\n\n\n**************************************************************");
+	printf("\n\t\t------WELCOME TO THE TELECOM BILLING MANAGEMENT SYSTEM---");
+	printf("\n\t\t****************************************************************");
+	Sleep(2000);
+	getch();
+    system("cls");
+	while (1)
+	{
+		system("cls");
+		printf("\n Enter\n A : for adding new records.\n L : for list of records");
+		printf("\n M : for modifying records.\n P : for payment");
+		printf("\n S : for searching records.");
+		printf("\n D : for deleting records.\n E : for exit\n");
+		choice=getche();
+		choice=toupper(choice);
+		switch(choice)
+		{
+
+			case 'D':
+				deleterecords();break;
+
+			default:
+				system("cls");
+				printf("Incorrect Input");
+				printf("\nAny key to continue");
+				getch();
+		}
+	}
+}
+
+
